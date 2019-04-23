cb48244950f06b0cdf30d766eb588ecebb5b95fa
mbaliu <38162096+mbaliu@users.noreply.github.com>
2019-04-23 12:48:13 -0300

    Update Filiacao_geosampa.py

--- PY/Filiacao_geosampa.py
+++ PY/Filiacao_geosampa.py
@@ -25,24 +25,30 @@ def load_page(site=''):
 	driver.get(site)
 
 def botao_pesquisar():
+	# FEcha a janela de boas-vindas
+	driver.find_element_by_xpath("//div[@class='close']").click()
+	
 	# Clica no botão PESQUISAR
-	driver.find_element_by_xpath("//*[@class='ctlBtnPesquisaLogradouroItemInactive olButton']")
+	driver.find_element_by_xpath("//*[@class='ctlBtnPesquisaLogradouroItemInactive olButton']").click()
 	
 	# Seleciona a ABA desejada - Integração
-	aba = 'IPTU'
-	driver.find_element_by_xpath("//area[@alt='{}']".format(aba))
+	aba = 'Integração'
+	driver.find_element_by_xpath("//area[@alt='{}']".format(aba)).click()
 
 	# Seleciona a OPÇÃO desejada - filiação
-	opcao = 'Filiação'
+	opcao = 'Filiação de Lotes'
 	driver.find_element_by_xpath("//select[@id='cboSerieIntegracao']/option[text()='{}']".format(opcao)).click()
 
+	
+	
 	'''
+	Códigos de referencia - obsoletos
 	driver.find_element_by_xpath("//select[@id='cboTipoMapaArticulado']/option[text()='Ortofotos 2004']").click()
 	driver.find_element_by_class_name('ctlBtnPesquisaLogradouroItemInactive olButton')
 	"//div[@id='OpenLayers_Control_Panel_535'][@title='Pesquisar']"
 	'''
 
-def set_input(sql='0001112222'):
+def set_input(sql='1112223333'):
 	# Completa com o SQL
 	
 
@@ -63,13 +69,48 @@ def set_input(sql='0001112222'):
 
 	# Solicita a procura do SQL
 	driver.find_element_by_xpath("//input[@id='BtnIntegracao']").click()
+	
 
 def get_data():
+	# CASO 1 - Não há registros
+	# Identifica que não há resposta
+	element = driver.find_element_by_xpath("//div[@id='popup_message']")
+	if element:
+		driver.find_element_by_xpath("//div[@id='popup_panel']/input[@type='button']").click()
+		continue
+		
+	# Identifica se tem resposta
+	element = driver.find_element_by_xpath("//div[@id='divDadosIntegracao']")
+	if element:
+		pass
+	
 	# Identifica os dados de PAI
-
+	element = driver.find_element_by_xpath("//div[@id='gbox_T_02824']") #funciona??? Verifica se existe a tabela de pai
+	if element:
+		# TABELA DE PAI
+		driver.find_element_by_xpath("//table[@id='T_02824']")
+		# Linha
+		("//table[@id='T_02824']/tbody/tr[@class='ui-widget-content jqgrow ui-row-ltr']")
+		# Encontra os campos da tabela
+		rows = driver.find_elements_by_xpath("//table[@id='T_02824']/tbody/tr[@class='ui-widget-content jqgrow ui-row-ltr']")
+		for r in rows: # Seleciona todas as linhas menos a primeira que é desnecessária
+			# Encontrará todas as células ==========================
+			cells = driver.find_elements_by_xpath("//table[@id='T_02824']/tbody/tr[@class='ui-widget-content jqgrow ui-row-ltr']/td")
+			for c in cells:
+				valor = c.text
+				
+	else:
+		# Não possui PAI
+		pass
 
 	# Identifica os dados de FILHO
 
 
 def save_data():
-	
+	pass
+
+
+'''
+range_v = valor.text.split(' ao ')
+lotes_range = [v for v in range(range_v[0], range_v[1]+1)]
+'''
